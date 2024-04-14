7e4fcc4a1edc342cd5c60778e9a5e6a9c087dccd
Lilian Gardon <lilian.gardon@gmail.com>
2024-04-14 03:24:21 -0400

    first implementation

--- app/src/main/java/uqac/dim/clockapp/ui/stopwatch/StopwatchFragment.java
+++ app/src/main/java/uqac/dim/clockapp/ui/stopwatch/StopwatchFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,17 +16,51 @@ import uqac.dim.clockapp.databinding.FragmentStopwatchBinding;
 public class StopwatchFragment extends Fragment {
 
     private FragmentStopwatchBinding binding;
+    private StopwatchViewModel stopwatchViewModel;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        StopwatchViewModel StopwatchViewModel =
-                new ViewModelProvider(this).get(StopwatchViewModel.class);
-
         binding = FragmentStopwatchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textStopwatch;
-        StopwatchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textViewTimer;
+        final Button startStopButton = binding.buttonStart;
+        final Button clearButton = binding.buttonStop;
+
+        stopwatchViewModel = new ViewModelProvider(this).get(StopwatchViewModel.class);
+
+        stopwatchViewModel.getElapsedTimeMillis().observe(getViewLifecycleOwner(), elapsedTime -> {
+            long seconds = elapsedTime / 1000;
+            long minutes = seconds / 60;
+            long hours = minutes / 60;
+
+            String timeString = String.format("%02d:%02d:%02d",
+                    hours % 24, minutes % 60, seconds % 60);
+
+            textView.setText(timeString);
+        });
+
+        startStopButton.setOnClickListener(view -> {
+            if (stopwatchViewModel.getElapsedTimeMillis().getValue() == null) {
+                // Start the stopwatch
+                stopwatchViewModel.startTimer();
+                startStopButton.setText("Stop");
+                clearButton.setEnabled(true);
+            } else {
+                // Stop the stopwatch
+                stopwatchViewModel.stopTimer();
+                startStopButton.setText("Start");
+            }
+        });
+
+        clearButton.setOnClickListener(view -> {
+            // Clear the stopwatch
+            stopwatchViewModel.resetTimer();
+            startStopButton.setText("Start");
+            clearButton.setEnabled(false);
+        });
+
         return root;
     }
 
--- app/src/main/java/uqac/dim/clockapp/ui/stopwatch/StopwatchViewModel.java
+++ app/src/main/java/uqac/dim/clockapp/ui/stopwatch/StopwatchViewModel.java
@@ -1,19 +1,55 @@
 package uqac.dim.clockapp.ui.stopwatch;
 
+import android.os.Handler;
+import android.os.SystemClock;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class StopwatchViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private MutableLiveData<Long> elapsedTimeMillis = new MutableLiveData<>();
+    private Handler timerHandler;
+    private Runnable timerRunnable;
 
     public StopwatchViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is Stopwatch fragment");
+        timerHandler = new Handler();
+        timerRunnable = new Runnable() {
+            @Override
+            public void run() {
+                long currentTime = SystemClock.elapsedRealtime();
+                long elapsedTime = currentTime - elapsedTimeMillis.getValue();
+                elapsedTimeMillis.setValue(elapsedTime);
+                timerHandler.postDelayed(this, 100); // Update every 100 milliseconds
+            }
+        };
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<Long> getElapsedTimeMillis() {
+        return elapsedTimeMillis;
+    }
+
+    public void startTimer() {
+        if (elapsedTimeMillis.getValue() == null) {
+            // Start a new timer
+            elapsedTimeMillis.setValue(SystemClock.elapsedRealtime());
+            timerHandler.postDelayed(timerRunnable, 100); // Start the timer
+        }
+    }
+
+    public void stopTimer() {
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
+    public void resetTimer() {
+        elapsedTimeMillis.setValue(null);
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        timerHandler.removeCallbacks(timerRunnable);
     }
 }
